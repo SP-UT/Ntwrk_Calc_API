a4d872b38007b97e99b9659306bd322fd5478f03
srinivaspradhan64 <srinivaspradhan@gmail.com>
2022-11-30 11:19:45 -0500

    Add Token validation code

--- app/cognito.py
+++ app/cognito.py
@@ -1,4 +1,5 @@
-import boto3
+import boto3, time
+from cognitojwt import decode
 
 client = boto3.client('cognito-idp')
 
@@ -16,6 +17,46 @@ def get_user_details(token: str):
         AccessToken=token
     )
     return(user_details)
+
+def get_jwt_user_details(token: str, region: str, idp_pool: str, client_id: str):
+    jwt_user_detail = decode(
+            token,
+            region,
+            idp_pool,
+            app_client_id=client_id
+        )
+    return (jwt_user_detail)
+
+def validate_user_id(token: str, region: str, idp_pool: str, client_id: str):
+    try:
+        idp_username = get_user_details(token)['Username']
+        jwt_username = get_jwt_user_details(
+            token,
+            region,
+            idp_pool,
+            app_client_id=client_id
+        )['username']
+    except Exception as e:
+        print(e)
+    if idp_username == jwt_username:
+        return {"user_verified": True}
+    else:
+        return {"user_verified": False}
+
+def is_token_expired(token: str, region: str, idp_pool: str, client_id: str):
+    try:
+        token_exp =  get_jwt_user_details(
+            token,
+            region,
+            idp_pool,
+            app_client_id=client_id
+        )['exp']
+    except Exception as e:
+        print(e)
+    if time.time() > token_exp:
+        return { "token_expired": True }
+    else:
+        return { "token_expired": False }
 
 def user_change_password(token: str, curr_password: str, new_password: str):
     passwd_change = client.change_password(
